51f908ed635ed9fea49257fc8fb8a0d110400124
titojlmm@gmail.com <titojlmm@gmail.com@f37a968a-be7c-7ed2-40be-f2805af42a7f>
2012-02-10 21:47:50 +0000

    Carga inicial

---
+++ src/titojlmm/Palabras.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package titojlmm;
+
+/**
+ *
+ * @author Tito
+ */
+public class Palabras {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Hola mundo");
+  }
+}
